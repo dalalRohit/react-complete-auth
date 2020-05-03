05608b43b7bf3da704ea216dbdababd917d0534d
Rohit Dalal <dalalrohit102@gmail.com>
2020-05-03 09:58:58 +0530

    Project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Complete user auth</h1>
+      
+      <ul>
+        <li> JWT Support </li>
+        <li> Email verification after signup </li>
+        <li> Password reset link </li>
+        <li> Remember Me </li>
+        <li> Session management </li>
+      </ul>
     </div>
   );
 }
